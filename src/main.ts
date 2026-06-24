c0b6b9af12d8287ea6e0db4c949f584a79a0fbf5
Zepyyy <quentin.stubecki@viacesi.fr>
2026-06-24 19:19:27 +0200

    fix: Allow patch requests to CORS

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 	app.setGlobalPrefix("api");
 	app.enableCors({
 		origin: ["https://coffyyy.quentinstubecki.fr", "http://localhost:5173"],
-		methods: ["GET", "POST", "PUT", "DELETE"],
+		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 		credentials: true,
 		allowedHeaders: ["Content-Type", "Authorization"],
 	});
