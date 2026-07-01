fed0d5e7d741502def495aa3591782cacbc776a0
Zepyyy <quentin.stubecki@viacesi.fr>
2026-07-01 22:13:50 +0200

    Protected all routes by default, except sign-in.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { APP_GUARD } from "@nestjs/core";
 import { JwtModule } from "@nestjs/jwt";
 import { UsersModule } from "../users/users.module";
 import { AuthController } from "./auth.controller";
+import { AuthGuard } from "./auth.guard";
 import { AuthService } from "./auth.service";
 
 @Module({
@@ -15,7 +17,7 @@ import { AuthService } from "./auth.service";
 			signOptions: { expiresIn: "30d" },
 		}),
 	],
-	providers: [AuthService],
+	providers: [AuthService, { provide: APP_GUARD, useClass: AuthGuard }],
 	controllers: [AuthController],
 	exports: [AuthService],
 })
